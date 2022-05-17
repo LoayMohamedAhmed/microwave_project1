7ab5788f94cf49fb7b01c41edc1e262a0b4f712b
ShoroukHegazy <soroukhegazy15@yahoo.com>
2022-05-17 19:35:56 +0200

    set_cursor

--- lcd.c
+++ lcd.c
@@ -13,3 +13,26 @@
 //	return;
 //}
 
+
+
+//void set_cursor(unsigned int x,unsigned int y) // where x is the row and y is the colum of the LCD Xrang(0-1) Yrang(1-16)
+//{
+//	int i;
+//  if (x==0)
+//	{ lcd_cmd(0X80); //access the first line of the LCD
+//    delay_ms(2);
+//	}
+//  else
+//	{
+//	  lcd_cmd(0XC1); // access the Second line of the LCD
+//    delay_ms(2);
+//	}
+
+//  for(i=0;i<y;i++)
+//  {
+//    lcd_cmd(0X14);
+//    delay_ms(2);
+//  }
+//}
+
+
