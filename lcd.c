58a56bc9a70ff4fa3ff921e9afc3a7b63b4aa0a2
LoayMohamedAhmed <looay432@gmail.com>
2022-05-17 18:04:34 +0200

    edit

---
+++ lcd.c
@@ -0,0 +1,15 @@
+#include "lcd.h"
+#include "ports_init.c"
+#include "delay.c"
+
+//void lcd_write(u8 data)
+//{
+//	GPIO_PORTB_DATA_R=data;
+//	GPIO_PORTE_DATA_R=0X0A;
+//	delay_ms(1);
+//	GPIO_PORTE_DATA_R=0X01;
+//	delay_ms(50);
+//	
+//	return;
+//}
+
---
+++ lcd.d
@@ -0,0 +1,6 @@
+.\lcd.o: lcd.c
+.\lcd.o: lcd.h
+.\lcd.o: ports_init.c
+.\lcd.o: headr.h
+.\lcd.o: D:\Program Files (x86)\ARM\ARMCC\bin\..\include\stdint.h
+.\lcd.o: delay.c
