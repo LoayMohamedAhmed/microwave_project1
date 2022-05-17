92d2871e795c16afe128106cb3eb8074f649adfd
nayera540 <69148381+nayera540@users.noreply.github.com>
2022-05-17 15:01:49 +0200

    delay_ms

--- lcd.h
+++ lcd.h
@@ -1,4 +1,4 @@
-typedef unsigned char u8;
+	typedef unsigned char u8;
 	typedef unsigned short int u16;
 	typedef unsigned long  int u32;
 	typedef signed char s8;
@@ -6,3 +6,5 @@ typedef unsigned char u8;
 	typedef signed long  int s32;
 	typedef float  f32;
 	typedef double f64;
+	
+	void delay_ms(int t);
