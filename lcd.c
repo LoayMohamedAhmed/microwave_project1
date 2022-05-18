0db1ba7f3fbdfc2273cae8f129bd9e101e3c3a5d
ShoroukHegazy <soroukhegazy15@yahoo.com>
2022-05-18 16:03:14 +0200

    LCD_Cmd_Char

--- lcd.c
+++ lcd.c
@@ -35,4 +35,20 @@
 //  }
 //}
 
+void LCD_Cmd(unsigned char command)
+{
+	GPIO_PORTA_DATA_R = 0x00; //RS=0, E=0, RW=0
+	GPIO_PORTA_DATA_R =command;
+	GPIO_PORTA_DATA_R =0x80; //E=1 to secure command
+	delay_micro(0);
+	GPIO_PORTA_DATA_R =0x00;
+	if(command <4) 
+	{
+		//delay_ms(2);
+	}	
+	else
+	{	
+		//delay_micro(37); 
+	}
+}
 
