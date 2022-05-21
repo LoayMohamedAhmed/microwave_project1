de32fea3c2823a6a6c590dda3e771ebe712a67bc
ShoroukHegazy15 <105971383+ShoroukHegazy15@users.noreply.github.com>
2022-05-22 01:34:48 +0200

    Enter_Kilos

--- state.c
+++ state.c
@@ -1,3 +1,28 @@
 #include "lcd.h"
 #include "state.h"
 extern char input[1];
+
+void Enter_Kilos()
+{
+	again:
+	lcd_cmd(0X01);
+	if(input[0]=='B')
+	{
+	  set_cursor(0,0); 
+    lcd_print("chicken weight:");
+		delay_ms(1000);
+	}
+	else if(input[0]=='C')
+	{
+		set_cursor(0,0); 
+    lcd_print("Beaf weight:");
+		delay_ms(1000);
+	}
+	set_cursor(1,8); 
+	lcd_print("KG");
+	set_cursor(1,7);
+	kilos=keypad_getkey();
+	delay_ms(700);
+	lcd_write(kilos);
+	delay_ms(1000);
+}
