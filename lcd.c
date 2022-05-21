afa51f43f4472cb1ed896bd8592922aeebed4aad
ShoroukHegazy <soroukhegazy15@yahoo.com>
2022-05-21 12:16:47 +0200

    edit3

--- lcd.c
+++ lcd.c
@@ -4,16 +4,16 @@
 #include "stdio.h"
 
 //For printing a character on the LCD
-void lcd_write(u8 data)
-{
-	GPIO_PORTB_DATA_R=data;
-	GPIO_PORTE_DATA_R=0X0A;
-	delay_ms(1);
-	GPIO_PORTE_DATA_R=0X01;
-	delay_ms(50);
-	
-	return;
-}
+//void lcd_write(u8 data)
+//{
+//	GPIO_PORTB_DATA_R=data;
+//	GPIO_PORTE_DATA_R=0X0A;
+//	delay_ms(1);
+//	GPIO_PORTE_DATA_R=0X01;
+//	delay_ms(50);
+//	
+//	return;
+//}
 
 
 //For printing a string on the LCD
