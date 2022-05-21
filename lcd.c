9cdb0c185a957222c6a50323b7430cb026af69d0
ShoroukHegazy <soroukhegazy15@yahoo.com>
2022-05-21 12:19:44 +0200

    edit4

--- lcd.c
+++ lcd.c
@@ -4,16 +4,16 @@
 #include "stdio.h"
 
 //For printing a character on the LCD
-//void lcd_write(u8 data)
-//{
-//	GPIO_PORTB_DATA_R=data;
-//	GPIO_PORTE_DATA_R=0X0A;
-//	delay_ms(1);
-//	GPIO_PORTE_DATA_R=0X01;
-//	delay_ms(50);
-//	
-//	return;
-//}
+void lcd_write(u8 data)
+{
+	GPIO_PORTB_DATA_R=data;
+	GPIO_PORTE_DATA_R=0X0A;
+	delay_ms(1);
+	GPIO_PORTE_DATA_R=0X01;
+	delay_ms(50);
+	
+	return;
+}
 
 
 //For printing a string on the LCD
@@ -42,26 +42,6 @@ void lcd_cmd(u8 cmd)
 	delay_ms(50);
 	
 	return;
-}
-
-void set_cursor(unsigned int x,unsigned int y)	// where x is the row and y is the colum of the LCD Xrange(0-1) Yrange(0-15)
-{
-	int i;
-  if (x==0)
-	{ lcd_cmd(0X80);															//access the first line of the LCD
-    delay_ms(2);
-	}
-  else
-	{
-	  lcd_cmd(0XC1);															// access the Second line of the LCD
-    delay_ms(2);
-	}
-
-  for(i=0;i<y;i++)
-  {
-    lcd_cmd(0X14);
-    delay_ms(2);
-  }
 }
 
 
