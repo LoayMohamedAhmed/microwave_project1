34cef286f98ebb53b21c900b8552ffbfd14fe8be
LoayMohamedAhmed <looay432@gmail.com>
2022-05-25 14:28:56 +0200

    edit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ char count_array1[4];
 char count_array[]={'0','0','0','0'};
 char input[1];
 char kilos;
-int m,k,j,i;
+int m,k,j,i,X;
 
 int main()
 {
--- state.c
+++ state.c
@@ -1,9 +1,11 @@
 #include "lcd.h"
+#include "headr.h"
+#include "stdint.h"
 #include "state.h"
 #include "keypad.h"
 extern char input[1];
 extern char kilos;
-extern 	int m,k,j,i;
+extern 	int m,k,j,i,X;
 
 void Enter_Kilos()
 {
@@ -46,59 +48,36 @@ void Enter_Kilos()
 }
 void GPIOF_Handler(){
 	
-	 int m,k,j,i;
-	 if (GPIOF->MIS & 0x01) /* check if interrupt causes by PF0/SW2*/
-	 {
-		if((count_array[0]>='3'&&(count_array[1]>'0'||count_array[2]>'0'||count_array[3]>'0'))||
-				(count_array[2]>='6')||(count_array[0]=='0'&&count_array[1]=='0'&&count_array[2]=='0'&&count_array[3]=='0'))
-			{
-				lcd_cmd(0x01);
-				set_cursor(0,1);
-				lcd_print("Invalid input!");
-				delay_ms(1000);
-				for(g=0;g<4;g++)
-				{
-					count_array1[g]='0';
-				}
-				goto end;
-			}
-		 
-	 }
-	  else if (GPIOF->MIS & 0x10)// check if interrupt causes by PF4/SW1 
-		{
-		
-		}
-		
-for( m = count_array[0]-'0'; m>=0; m--){//Counting down the first digit in minutes
+	for( m = count_array[0]-'0'; m>=0; m--){//Counting down the first digit in minutes
   
   for( k= count_array[1]-'0'; k>=0; k--){//Counting down the second digit in minutes
       
        for(j=count_array[2]-'0'; j>=0;j--){//Counting down the first digit in seconds
        
-					for( i = count_array[3]-'0'; i>=0; i--){//Counting down the second digit in seconds
+         for( i = count_array[3]-'0'; i>=0; i--){//Counting down the second digit in seconds
              set_cursor(1,6);
-						 LCD_printInt(m);
-             LCD_printInt(k);
-             lcd_write(':');
-             LCD_printInt(j);
-             LCD_printInt(i);
-						  while(GPIOD->DATA==0x00){
-							 if(GPIOD->DATA==0x80){break;}//if door was opened
-						 }
-				     delay_ms(900);
-			
+        LCD_printInt(m);
+        LCD_printInt(k);
+        lcd_write(':');
+        LCD_printInt(j);
+        LCD_printInt(i);
+      delay_ms(900);
+          
+    while(GPIOD->DATA==0x00)
+{
+  if(GPIOD->DATA==0x80){break;}
+}
 
-
-						if(GPIO_PORTF_DATA_R ==0x0D) //if pause switch is pressed
-				 { 
+    if(GPIO_PORTF_DATA_R ==0x0D){
       while(GPIO_PORTF_DATA_R != 0x1C){
 				GPIOD->DATA=0X40;
 				GPIOF->DATA&=~0X0C;
         delay_ms(700);
 				GPIOF->DATA|=0X0C;
 				set_cursor(0,3);
-        while(GPIO_PORTF_DATA_R==0X0D) // if pause switch is pressed again ->stop
+        while(GPIO_PORTF_DATA_R==0X0D)
         { 
+					
           i=3;
           X=1;
           Y=1;
@@ -106,36 +85,55 @@ for( m = count_array[0]-'0'; m>=0; m--){//Counting down the first digit in minut
           Z=1;
           goto end;
         }
-	GPIOF->DATA=0x00;
-				i=3;
-				X=1;
-				Y=1;
-				W=1;
-				Z=1;
-GPIO_PORTD_DATA_R=0X40;
-	lcd_cmd(0X01);
-	lcd_cmd(0x02);
-	lcd_print("cooking Done");
+          
+        
+        
+      }
+			GPIOD->DATA=0x00;
+  }
+     
+         }
+       count_array[3]='9';
+       }
+    count_array[2] = '5';
+     }
+  count_array[1] = '9';
 }
-							
-							
-while(i)
-{ 
-    GPIOF->DATA =0X02;
-    GPIO_PORTD_DATA_R=0X40;
+GPIOF->DATA=0x00;
+i=3;
+X=1;
+Y=1;
+W=1;
+Z=1;
+GPIO_PORTD_DATA_R=0X40;
+lcd_cmd(0X01);
+ lcd_cmd(0x02);
+ lcd_print("cooking Done");
+      while(i)
+  { 
+		GPIOF->DATA =0X02;
+		GPIO_PORTD_DATA_R=0X40;
     delay_ms(200);
     GPIOF->DATA =0X04;
     delay_ms(200);
     GPIOF->DATA =0X08;
-    GPIO_PORTD_DATA_R=0X00;
+		GPIO_PORTD_DATA_R=0X00;
     delay_ms(200);
     GPIOF->DATA =0X00;
-    set_cursor(0,5);
+     set_cursor(0,5);
     i--;
+    
   }
- /* clear the interrupt flag */
-		end:
+   
+    /* clear the interrupt flag */
+  end:
     GPIO_PORTF_ICR_R|= 0x01;
-//	Reset_Timer();
+//	   Reset_Timer();
+    }
+
+	else if (GPIOF->MIS & 0x10)// check if interrupt causes by PF4/SW1 
+		{
+		
+		}
     }
       
